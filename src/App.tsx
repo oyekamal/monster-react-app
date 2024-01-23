6b5892dd0c68a2183f3b05fe0fb6329ae158d71a
oyekamal <autocratickamal1@gmail.com>
2024-01-23 14:43:53 +0500

    ui updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,63 @@
-import Form from "react-bootstrap/Form";
-import ListGroup from "react-bootstrap/ListGroup";
-import Button from "react-bootstrap/Button";
-import { useRef, useState, useEffect } from "react";
+import {
+  Container,
+  FormControl,
+  Card,
+  InputGroup,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  Button,
+} from "react-bootstrap";
 
 function App() {
-  const [display, setDisplay] = useState([]);
-  const [apidata, setApidata] = useState([]);
-
-  const inputRef = useRef();
-
-  const gettext = () => {
-    let search = inputRef.current.value;
-    const filterItems = (arr, query) =>
-      arr.filter(
-        (element: object) =>
-          element.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
-      );
-
-    if (search === "" || search === undefined) {
-      setDisplay(apidata);
-    } else {
-      setDisplay(filterItems(apidata, search));
-    }
-  };
-
-  useEffect(() => {
-    const url = "https://jsonplaceholder.typicode.com/users";
-    console.log("hit");
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log(json);
-        setApidata(json);
-        setDisplay(json);
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <>
-      <input
-        ref={inputRef}
-        placeholder="search"
-        aria-label="search"
-        id="search"
-        onChange={gettext}
-      />
-      <Button variant="btn-primary">Button</Button>
-      <ListGroup variant="flush">
-        {display.map((item, index) => (
-          <ListGroup.Item>{item.name}</ListGroup.Item>
-        ))}
-      </ListGroup>
+      <Container fluid>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Monster Store</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Card className="text-center">
+          <Card.Header>
+            <h1>Monster Shop</h1>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>We sell every type of Monster here</Card.Title>
+            <Card.Text>
+              for all type of needs, we got you covered from stronge to cute any
+              type.
+            </Card.Text>
+            <div>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="Monster Search"
+                  aria-label="Monster Search"
+                  aria-describedby="basic-addon1"
+                />
+              </InputGroup>
+            </div>
+            <br />
+            <div></div>
+          </Card.Body>
+        </Card>
+
+        <div>
+          <br />
+          <Card className="text-center">
+            <Card.Footer className="text-muted">
+              created by: oykamal
+            </Card.Footer>
+          </Card>
+        </div>
+      </Container>
     </>
   );
 }
